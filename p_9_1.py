9642da6a372c100aef119f17552e0491c8a7306e
Quoc Anh Tran <quoc.honza@gmail.com>
2017-12-13 17:09:48 +0000

    Solved 9-4

--- p_9_1.py
+++ p_9_1.py
@@ -1,10 +1,11 @@
 
 
 class BinaryTreeNode:
-    def __init__(self, data=None, left=None, right=None):
+    def __init__(self, data=None, left=None, right=None, parent=None):
         self.data = data
         self.left = left
         self.right = right
+        self.parent = parent
 
 def test_height_balanced(root):
 
