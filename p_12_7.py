4b409dd9e1d7a44892f3ea733249b28ae0dafdb3
Quoc Anh Tran <quoc.honza@gmail.com>
2018-01-25 16:33:55 +0000

    Improved 12-7

--- p_12_7.py
+++ p_12_7.py
@@ -1,10 +1,6 @@
 import sys
 
 def find_smallest_subset(A, S):
-    character_positions = []
-    for i, x in enumerate(A):
-        if x in S:
-            character_positions.append((i, x))
     
     subset_character_count = {}
     head = tail = 0
@@ -12,12 +8,13 @@ def find_smallest_subset(A, S):
     
     while True:
         # Moving tail forward
-        while (len(subset_character_count) < len(S)) and (tail < len(character_positions)):
-            curr = character_positions[tail][1]
-            if curr not in subset_character_count:
-                subset_character_count[curr] = 1
-            else:
-                subset_character_count[curr] += 1
+        while (len(subset_character_count) < len(S)) and (tail < len(A)):
+            curr = A[tail]
+            if curr in S:
+                if curr not in subset_character_count:
+                    subset_character_count[curr] = 1
+                else:
+                    subset_character_count[curr] += 1
             tail += 1
             
         # Check for the end of array
@@ -26,21 +23,22 @@ def find_smallest_subset(A, S):
             
         # Moving head forward
         while (len(subset_character_count) == len(S)) and (head < tail):
-            curr = character_positions[head][1]
-            subset_character_count[curr] -= 1
-            if subset_character_count[curr] == 0:
-                del subset_character_count[curr]
+            curr = A[head]
+            if curr in S:
+                subset_character_count[curr] -= 1
+                if subset_character_count[curr] == 0:
+                    del subset_character_count[curr]
             head += 1
 
         # Compare current subset with the previous best subset
         print('head: {}, tail: {}'.format(head, tail))
-        start_index, end_index = character_positions[head-1][0], character_positions[tail-1][0]
-        if (end_index - start_index) < (result[1] - result[0]):
-            result = (start_index, end_index)
+        if (tail - head) < (result[1] - result[0]):
+            result = (head, tail)
             
-    return result
+    # Adjust result
+    return (result[0] - 1, result[1] - 1)
     
 A = ['A', 'B', 'A', 'C', 'D', 'E', 'E', 'E', 'F', 'A', 'B', 'E']
-S = set(['E', 'A'])
+S = set(['E', 'D', 'F'])
 
 print(find_smallest_subset(A, S))
