6e3c763e2ec9750e56f9d4c56e28695b45246df6
hotelCA <quoc.honza@gmail.com>
2018-03-24 07:44:51 -0700

    Solved 13-5

--- p_13_5.py
+++ p_13_5.py
@@ -1,24 +1,37 @@
 import collections
 
 Interval = collections.namedtuple('Interval', ('start', 'end'))
-
+EndpointAndTime = collections.namedtuple('EndpointAndTime', ('value', 'type'))
 
 def find_largest_overlap(A):
-    largest = 0
-        
-    for i in range(0,24):
-        current_hour_overlap = 0
-        for interval in A:
-            if interval.start <= i and interval.end >= i:
-                current_hour_overlap += 1
-        if current_hour_overlap > largest:
-            largest = current_hour_overlap
-    return largest
-    
+    endpoints = []
+
+    # prepare endpoints
+    for interval in A:
+        endpoints.append(EndpointAndTime(value=interval.start, type='start'))
+        endpoints.append(EndpointAndTime(value=interval.end, type='end'))
+
+    running_sum = 0
+    max_overlap = 0
+
+    # enumerate through the sorted endpoints
+    for endpoint in sorted(endpoints):
+        print(endpoint.type)
+        if endpoint.type == 'start':
+            running_sum += 1
+        else:
+            running_sum -= 1
+
+        max_overlap = max(running_sum, max_overlap)
+
+    print(max_overlap)
+
+
 A = [Interval(1,2),
     Interval(2,4),
     Interval(3,6),
     Interval(4,8),
     Interval(7,8),
     Interval(10,21)]
+
 print(find_largest_overlap(A))
